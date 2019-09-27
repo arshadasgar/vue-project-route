df94a832735e5737d8de82dc6217ed1659dd3a09
arshadmuhammed <arshadmuhammed@qburst.com>
2019-09-27 17:36:44 +0530

    Channge helloworld to Home

--- todo-route/src/router/index.js
+++ todo-route/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import Todo from '@/components/Todo'
 import TodoItemView from '@/components/TodoItemView'
 import VuexExample from '@/components/VuexExample'
@@ -11,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     },
     {
       path: '/todo',
